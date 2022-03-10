e2f691a4464dd1d736dd740a5892eae29e64b746
Jeroen Gunster <jeroen.gunster@gmx.com>
2022-03-10 19:43:58 +0100

    chore: add and inject releases service

--- src/releases/releases.controller.ts
+++ src/releases/releases.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { ReleasesService } from './releases.service';
 
 @Controller('releases')
-export class ReleasesController {}
+export class ReleasesController {
+  constructor(private releasesService: ReleasesService) {}
+}
---
+++ src/releases/releases.service.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { ReleasesService } from './releases.service';
+
+describe('SeminarsService', () => {
+  let service: ReleasesService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ReleasesService],
+    }).compile();
+
+    service = module.get<ReleasesService>(ReleasesService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
