f2cf1517b6b3e95ea7a9e59417df30cc0680e1d7
Jeroen Gunster <jeroen.gunster@gmx.com>
2022-03-10 19:41:46 +0100

    chore: add releases controller + module

---
+++ src/releases/releases.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { ReleasesController } from './releases.controller';
+
+@Module({
+  controllers: [ReleasesController],
+})
+export class ReleasesModule {}
